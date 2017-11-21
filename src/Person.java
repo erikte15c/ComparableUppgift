ffa55b7b4d030421bb46da74dd66d4a7e87e3a14
erikte15c <erik.danielsson.te15d@gmail.com>
2017-11-21 10:18:37 +0100

    Genererade getters och setters

--- src/Person.java
+++ src/Person.java
@@ -4,6 +4,22 @@ public class Person implements Comparable<Person>{
 	private String firstName;
 	private String lastName;
 	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
 	@Override
 	public int compareTo(Person arg0) {
 		// TODO Auto-generated method stub
