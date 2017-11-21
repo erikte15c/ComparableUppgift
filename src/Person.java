24d12c7959637abe24237b069f05b5eb0f5db58e
erikte15c <erik.danielsson.te15d@gmail.com>
2017-11-21 10:17:43 +0100

    Class person variabler

---
+++ src/Person.java
@@ -0,0 +1,13 @@
+
+public class Person implements Comparable<Person>{
+
+	private String firstName;
+	private String lastName;
+	
+	@Override
+	public int compareTo(Person arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
