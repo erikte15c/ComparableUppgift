911c9ff029e44584d613bd8e94a1e64852d58f0c
erikte15c <erik.danielsson.te15d@gmail.com>
2017-11-21 10:58:45 +0100

    Scanner och arraylist

--- src/RunPerson.java
+++ src/RunPerson.java
@@ -1,4 +1,18 @@
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class RunPerson {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String Namn = sc.nextLine();
+		
+		ArrayList<Person> person = new ArrayList<Person>();
+		
+	}
 
 }
